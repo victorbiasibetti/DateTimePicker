a05ad9bf8f750a2d785c346485378f837f369827
Victor Biasibetti <victorbiasibetti7@gmail.com>
2026-04-30 11:11:11 -0300

    chore(test): set up vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.ts'],
+    globals: false,
+  },
 })
